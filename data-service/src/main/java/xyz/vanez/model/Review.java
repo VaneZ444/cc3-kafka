f6f9c89d6166b596ce69576d13a1994e90bb05a9
VaneZ <bsaaa444@gmail.com>
2026-05-07 04:42:54 +0400

    Create Review.java

---
+++ data-service/src/main/java/xyz/vanez/model/Review.java
@@ -0,0 +1,30 @@
+package xyz.vanez.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "reviews")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
+
+    private Integer rating;
+
+    private String comment;
+
+    private LocalDateTime createdAt;
+}
