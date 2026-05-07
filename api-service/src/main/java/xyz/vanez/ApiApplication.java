7c4055fdcdf96c075a389784dd4c6860a2bb36e5
VaneZ <bsaaa444@gmail.com>
2026-05-07 04:38:10 +0400

    Create ApiApplication.java

---
+++ api-service/src/main/java/xyz/vanez/ApiApplication.java
@@ -0,0 +1,19 @@
+package xyz.vanez;
+
+import lombok.Getter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
