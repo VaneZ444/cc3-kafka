69a4ada8f521c769e610abe9ca5c4dc0dda48a09
VaneZ <bsaaa444@gmail.com>
2026-05-07 04:44:02 +0400

    Create ReviewService.java

---
+++ data-service/src/main/java/xyz/vanez/service/ReviewService.java
@@ -0,0 +1,63 @@
+package xyz.vanez.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import xyz.vanez.dto.ReviewRequest;
+import xyz.vanez.model.Restaurant;
+import xyz.vanez.model.Review;
+import xyz.vanez.repository.RestaurantRepository;
+import xyz.vanez.repository.ReviewRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ReviewService {
+
+    private final RestaurantRepository restaurantRepository;
+    private final ReviewRepository reviewRepository;
+
+    public void saveReview(ReviewRequest req) {
+        log.debug("Saving Review: {}", req);
+        Restaurant restaurant = restaurantRepository.findByName(req.getRestaurantName())
+                .orElseGet(() -> {
+                    log.info("Created new restaurant: {}", req.getRestaurantName());
+                    Restaurant r = new Restaurant();
+                    r.setName(req.getRestaurantName());
+                    r.setAddress(req.getRestaurantAddress());
+                    return restaurantRepository.save(r);
+                });
+        Review review = new Review();
+        review.setRestaurant(restaurant);
+        review.setRating(req.getRating());
+        review.setComment(req.getComment());
+        review.setCreatedAt(LocalDateTime.now());
+        reviewRepository.save(review);
+        log.info("Review saved: restaurant '{}', rating {}", restaurant.getName(), req.getRating());
+    }
+
+    public List<Review> searchReviews(String text) {
+        log.debug("searchReviews: {}", text);
+        return reviewRepository.findByCommentContainingIgnoreCase(text);
+    }
+
+    public List<Object[]> getTopRated() {
+        log.debug("getTopRated");
+        return reviewRepository.findTopRatedRestaurants(PageRequest.of(0, 10));
+    }
+
+    public List<Object[]> getReviewsByDay() {
+        log.debug("getReviewsByDay");
+        return reviewRepository.getReviewsCountByDay();
+    }
+
+    public List<Object[]> getMostActive() {
+        log.debug("getMostActive");
+        return reviewRepository.findMostActiveRestaurants(PageRequest.of(0, 10));
+    }
+}
